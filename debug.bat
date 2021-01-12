8b78cf4746f3c664665e90900c26b2ddb0eeb162
JulianBohne <julianbohnenkaemper@gmail.com>
2021-01-12 15:31:15 +0100

    Fix versuch 2

--- debug.bat
+++ debug.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Trying to compile and assemble...
-g++ -Wall -c src/TestGL.cpp -o object_files/TestGL.o -I dependencies/glew/include -I dependencies/glfw/include -static-libgcc -static-libstdc++
+g++ -Wall -c src/TestGL.cpp -o object_files/TestGL.o -I dependencies/glew/include -I dependencies/glfw/include
 echo Trying to link...
-g++ -Wall object_files/TestGL.o -o debug/TestGL.exe -lglew32s -lglfw3 -lgdi32 -luser32 -lopengl32 -lkernel32 -L dependencies/glew/lib/Release/Win32 -L dependencies/glfw/lib-mingw
+g++ -Wall object_files/TestGL.o -o debug/TestGL.exe -static-libgcc -static-libstdc++ -lglew32s -lglfw3 -lgdi32 -luser32 -lopengl32 -lkernel32 -L dependencies/glew/lib/Release/Win32 -L dependencies/glfw/lib-mingw
 echo Done trying :D
 echo Start the program?
 pause
