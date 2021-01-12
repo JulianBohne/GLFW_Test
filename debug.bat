f4cad3bb26d65b209e20029be383c07851121b94
JulianBohne <julianbohnenkaemper@gmail.com>
2021-01-12 15:28:14 +0100

    Fix for dll missing issue 1

--- debug.bat
+++ debug.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Trying to compile and assemble...
-g++ -Wall -c src/TestGL.cpp -o object_files/TestGL.o -I dependencies/glew/include -I dependencies/glfw/include
+g++ -Wall -c src/TestGL.cpp -o object_files/TestGL.o -I dependencies/glew/include -I dependencies/glfw/include -static-libgcc -static-libstdc++
 echo Trying to link...
 g++ -Wall object_files/TestGL.o -o debug/TestGL.exe -lglew32s -lglfw3 -lgdi32 -luser32 -lopengl32 -lkernel32 -L dependencies/glew/lib/Release/Win32 -L dependencies/glfw/lib-mingw
 echo Done trying :D
